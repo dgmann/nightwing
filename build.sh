3c9dec98c43c73d2dd02b088456532464cb191b1
dgmann <dgmann@users.noreply.github.com>
2024-11-15 20:31:26 +0100

    feat: add mpv

--- build.sh
+++ build.sh
@@ -44,7 +44,8 @@ rpm-ostree install kitty \
       containerd.io \
       docker-buildx-plugin \
       docker-compose-plugin \
-      sunshine
+      sunshine \
+      mpv
 
 rpm-ostree override remove tailscale
 
